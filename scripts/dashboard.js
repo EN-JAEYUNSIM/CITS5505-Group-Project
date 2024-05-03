96cbf330c75b4d64e8639aaf07aee30a36dac76d
leah1leah1 <liuxiaoyi.xyl@gmail.com>
2024-05-04 02:54:15 +0800

    Added the dashboard page
    I added the dashboard page documents, and adjust the same type documents to the respective folders for easier management later. Just let me know if there is any problem.

---
+++ scripts/dashboard.js
@@ -0,0 +1,32 @@
+// Change user status 
+$(document).ready(function() {
+    $.get('/user-status', function(response) {
+        if (response.logged_in) {
+            $('#user-links').html('<a href="/my-account">My Account</a>');
+        } else {
+            $('#user-links').html('<a href="/login">Log In</a> <a href="/signup">Sign Up</a>');
+        }
+    });
+});
+
+// Following/Trending/Waitforanswering tags
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className  += " active";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.tablink[onclick*="Trending"]').click();
+});
+
