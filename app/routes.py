b84c8a41ce2d6e6fcfb13ee790f6ebc6da42f67a
leah1leah1 <liuxiaoyi.xyl@gmail.com>
2024-05-10 01:55:54 +0800

    adjust dynamic content in html files and create base,html for extends

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from app import app, db
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app.models import User
+from werkzeug.security import generate_password_hash
+from app.forms import SignUpForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html', title='Sign Up')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', title='Dashboard')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
