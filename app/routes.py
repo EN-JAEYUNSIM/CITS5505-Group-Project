97a6fd8f6c3418842a75c14360e5cc269ea18587
leah1leah1 <liuxiaoyi.xyl@gmail.com>
2024-05-10 19:32:22 +0800

    Add user authentication for login and sign up
    - Add login and signup functionality in routes.py and forms.py using Flask.
    - Create Jinja templates for forms with CSRF protection.
    - Add error messages and redirects.

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,48 @@
 from app import app, db
-from flask import render_template, redirect, url_for, request, flash
-from flask_login import current_user, login_user
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm, SignupForm
+from flask_login import current_user, login_user, logout_user
 import sqlalchemy as sa
 from app.models import User
-from werkzeug.security import generate_password_hash
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title='Login')
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('dashboard'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Log In', form=form)
 
-@app.route('/signup')
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html', title='Sign Up')
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you signed up successfully!')
+        return redirect(url_for('login'))
+    return render_template('signup.html', title='Singn Up', form=form)
 
 @app.route('/dashboard')
 def dashboard():
