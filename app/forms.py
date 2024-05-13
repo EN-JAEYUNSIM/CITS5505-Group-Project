82f824d446450b3313404ce49640df50003deced
leah1leah1 <liuxiaoyi.xyl@gmail.com>
2024-05-13 23:55:53 +0800

    add CommentForm to forms.py and make a details.html file for post details

--- app/forms.py
+++ app/forms.py
@@ -28,3 +28,7 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = StringField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+class CommentForm(FlaskForm):
+    content = StringField('Content', validators=[DataRequired()])
+    submit = SubmitField('Comment')
